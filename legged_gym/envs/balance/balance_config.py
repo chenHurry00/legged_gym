0247eab4d5bd1ec9b5aa8bf234834c669d5a2e3d
yuchen <1163581142@qq.com>
2025-08-19 21:00:43 +0800

    Improve result.

--- legged_gym/envs/balance/balance_config.py
+++ legged_gym/envs/balance/balance_config.py
@@ -5,7 +5,7 @@ from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobot
 
 class BalanceCfg(LeggedRobotCfg):
     class env(LeggedRobotCfg.env):
-        num_envs = 8192
+        num_envs = 4096
         '''
         线速度 (1): 只有前进方向(x)的线速度，因为平衡车无法横向移动
         角速度 (2): pitch角速度(倾斜)和yaw角速度(转向)
@@ -18,10 +18,10 @@ class BalanceCfg(LeggedRobotCfg):
         num_observations = 14  # 观测数量
         num_privileged_obs = None
         num_actions = 2  # 两个轮子的控制
-        episode_length_s = 20  # 每个episode的时长(秒)
+        episode_length_s = 30  # 每个episode的时长(秒)
 
     class init_state(LeggedRobotCfg.init_state):
-        pos = [0.0, 0.0, 0.3]  # 起始高度
+        pos = [0.0, 0.0, 0.225]  # 起始高度
         rot = [0.0, 0.0, 0.0, 1.0]  # 初始为直立姿态
         lin_vel = [0.0, 0.0, 0.0]
         ang_vel = [0.0, 0.0, 0.0]
@@ -43,30 +43,30 @@ class BalanceCfg(LeggedRobotCfg):
         curriculum = False
         max_curriculum = 1.
         num_commands = 3 # default: lin_vel_x, ang_vel_yaw, heading (in heading mode ang_vel_yaw is recomputed from heading error)
-        resampling_time = 10. # time before command are changed[s]
-        heading_command = False # if true: compute ang vel command from heading error
+        resampling_time = 5. # time before command are changed[s]
+        heading_command = True # if true: compute ang vel command from heading error
         class ranges:
-            lin_vel_x = [-3.0, 3.0] # min max [m/s]
-            ang_vel_yaw = [-1, 1]    # min max [rad/s]
+            lin_vel_x = [-1.0, 1.0] # min max [m/s]
+            ang_vel_yaw = [-3.14, 3.14]    # min max [rad/s]
             heading = [-3.14, 3.14]
 
     class asset(LeggedRobotCfg.asset):
         file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/balance/urdf/balance.urdf'
         name = "balance"
         foot_name = "wheel"  # 轮子作为"足部"检测接触
-        penalize_contacts_on = ["base_link"]  # 底盘接触地面应该被惩罚
+        penalize_contacts_on = []  # 底盘接触地面应该被惩罚
         terminate_after_contacts_on = ["base_link"]  # 底盘接触地面应该终止
         self_collisions = 1  # 1禁用，0启用自碰撞
 
     class rewards(LeggedRobotCfg.rewards):
-        soft_dof_pos_limit = 0.9
-        base_height_target = 0.26  # 车轮半径+些许余量
+        base_height_target = 0.225  # 车轮半径+些许余量
         upright_target = [0, 0, 1]  # z轴朝上
+        only_positive_rewards = False
 
         class scales(LeggedRobotCfg.rewards.scales):
-            torques = -0.0002
-            dof_pos_limits = -10.0
-            orientation = 10.0  # 直立姿态奖励
+            torques = -0.0001
+            dof_pos_limits = 0
+            orientation = -10.0  # 直立姿态奖励
             tracking_lin_vel = 2.0  # 前进速度跟踪
             tracking_ang_vel = 1.0  # yaw角速度跟踪奖励
             collision = -5.0  # 增加碰撞惩罚
@@ -83,5 +83,5 @@ class BalanceCfgPPO( LeggedRobotCfgPPO ):
         run_name = ''
         experiment_name = 'rough_balance'
 
-        max_iterations = 1200 # number of policy updates
+        max_iterations = 1500 # number of policy updates
   
--- legged_gym/envs/balance/balance_tsk.py
+++ legged_gym/envs/balance/balance_tsk.py
@@ -86,23 +86,17 @@ class Balance(LeggedRobot):
         
         # 为每个观测分量设置噪声水平
         # 线速度 (索引0)
-        noise_vec[0] = noise_scales.lin_vel * noise_level * self.obs_scales.lin_vel
-        
+        noise_vec[0] = 0.#noise_scales.lin_vel * noise_level * self.obs_scales.lin_vel
         # 角速度 (索引1-2)
         noise_vec[1:3] = noise_scales.ang_vel * noise_level * self.obs_scales.ang_vel
-        
         # 投影重力 (索引3-5)
         noise_vec[3:6] = noise_scales.gravity * noise_level
-        
         # 命令 - 通常不添加噪声 (索引6-7)
         noise_vec[6:8] = 0.
-        
         # 轮子位置 (索引8-9)
         noise_vec[8:10] = noise_scales.dof_pos * noise_level * self.obs_scales.dof_pos
-        
         # 轮子速度 (索引10-11)
         noise_vec[10:12] = noise_scales.dof_vel * noise_level * self.obs_scales.dof_vel
-        
         # 上一步动作 - 通常不添加噪声 (索引12-13)
         noise_vec[12:14] = 0.
         
@@ -140,53 +134,53 @@ class Balance(LeggedRobot):
         if self.cfg.domain_rand.push_robots and  (self.common_step_counter % self.cfg.domain_rand.push_interval == 0):
             self._push_robots()
 
-    def _reset_dofs(self, env_ids):
-        """重写DOF重置方法，给予轻微的随机初始姿态，促进探索"""
-        # 对车轮位置进行随机初始化
-        self.dof_pos[env_ids] = self.default_dof_pos * torch_rand_float(0.9, 1.1, (len(env_ids), self.num_dof), device=self.device)
-        # 对车轮速度进行随机初始化
-        self.dof_vel[env_ids] = torch_rand_float(-0.1, 0.1, (len(env_ids), self.num_dof), device=self.device)
-        
-        # 更新物理引擎
-        env_ids_int32 = env_ids.to(dtype=torch.int32)
-        self.gym.set_dof_state_tensor_indexed(self.sim,
-                                             gymtorch.unwrap_tensor(self.dof_state),
-                                             gymtorch.unwrap_tensor(env_ids_int32), len(env_ids_int32))
+    # def _reset_dofs(self, env_ids):
+    #     """重写DOF重置方法，给予轻微的随机初始姿态，促进探索"""
+    #     # 对车轮位置进行随机初始化
+    #     self.dof_pos[env_ids] = self.default_dof_pos * torch_rand_float(0.9, 1.1, (len(env_ids), self.num_dof), device=self.device)
+    #     # 对车轮速度进行随机初始化
+    #     self.dof_vel[env_ids] = torch_rand_float(-0.1, 0.1, (len(env_ids), self.num_dof), device=self.device)
+    #
+    #     # 更新物理引擎
+    #     env_ids_int32 = env_ids.to(dtype=torch.int32)
+    #     self.gym.set_dof_state_tensor_indexed(self.sim,
+    #                                          gymtorch.unwrap_tensor(self.dof_state),
+    #                                          gymtorch.unwrap_tensor(env_ids_int32), len(env_ids_int32))
     
-    def _reset_root_states(self, env_ids):
-        """重写根状态重置方法，给予轻微的随机初始姿态"""
-        # 基础位置和姿态
-        self.root_states[env_ids] = self.base_init_state
-        self.root_states[env_ids, :3] += self.env_origins[env_ids]
-        
-        # 添加轻微的随机旋转（围绕侧向轴）以便机器人需要自行平衡
-        rand_euler = torch_rand_float(-0.15, 0.15, (len(env_ids), 3), device=self.device)
-        rand_euler[:, 0] = 0  # 只在侧向轴添加随机性
-        rand_euler[:, 2] = 0
-        rand_quat = quat_from_euler_xyz(rand_euler[:, 0], rand_euler[:, 1], rand_euler[:, 2])
-        self.root_states[env_ids, 3:7] = rand_quat
-        
-        # 添加轻微的随机速度
-        self.root_states[env_ids, 7:13] = torch_rand_float(-0.1, 0.1, (len(env_ids), 6), device=self.device)
-        
-        # 更新物理引擎
-        env_ids_int32 = env_ids.to(dtype=torch.int32)
-        self.gym.set_actor_root_state_tensor_indexed(self.sim,
-                                                    gymtorch.unwrap_tensor(self.root_states),
-                                                    gymtorch.unwrap_tensor(env_ids_int32), len(env_ids_int32))
+    # def _reset_root_states(self, env_ids):
+    #     """重写根状态重置方法，给予轻微的随机初始姿态"""
+    #     # 基础位置和姿态
+    #     self.root_states[env_ids] = self.base_init_state
+    #     self.root_states[env_ids, :3] += self.env_origins[env_ids]
+    #
+    #     # 添加轻微的随机旋转（围绕侧向轴）以便机器人需要自行平衡
+    #     rand_euler = torch_rand_float(-0.15, 0.15, (len(env_ids), 3), device=self.device)
+    #     rand_euler[:, 0] = 0  # 只在侧向轴添加随机性
+    #     rand_euler[:, 2] = 0
+    #     rand_quat = quat_from_euler_xyz(rand_euler[:, 0], rand_euler[:, 1], rand_euler[:, 2])
+    #     self.root_states[env_ids, 3:7] = rand_quat
+    #
+    #     # 添加轻微的随机速度
+    #     self.root_states[env_ids, 7:13] = torch_rand_float(-0.1, 0.1, (len(env_ids), 6), device=self.device)
+    #
+    #     # 更新物理引擎
+    #     env_ids_int32 = env_ids.to(dtype=torch.int32)
+    #     self.gym.set_actor_root_state_tensor_indexed(self.sim,
+    #                                                 gymtorch.unwrap_tensor(self.root_states),
+    #                                                 gymtorch.unwrap_tensor(env_ids_int32), len(env_ids_int32))
     
-    def _reward_orientation(self):
-        """重写姿态奖励，鼓励平衡车保持直立"""
-        # 使用投影重力来衡量直立程度
-        # 投影重力的x分量表示前后倾斜度
-        forward_gravity = self.projected_gravity[:, 0]
-        # 投影重力的y分量表示左右倾斜度
-        lateral_gravity = self.projected_gravity[:, 1]
-        
-        # 计算与理想直立状态的差距
-        up_reward = torch.exp(-torch.sum(torch.square(self.projected_gravity[:, :2]), dim=1) / 0.15)
-        
-        return up_reward
+    # def _reward_orientation(self):
+    #     """重写姿态奖励，鼓励平衡车保持直立"""
+    #     # 使用投影重力来衡量直立程度
+    #     # 投影重力的x分量表示前后倾斜度
+    #     forward_gravity = self.projected_gravity[:, 0]
+    #     # 投影重力的y分量表示左右倾斜度
+    #     lateral_gravity = self.projected_gravity[:, 1]
+    #
+    #     # 计算与理想直立状态的差距
+    #     up_reward = torch.exp(-torch.sum(torch.square(self.projected_gravity[:, :2]), dim=1) / 0.15)
+    #
+    #     return up_reward
     
     def _reward_tracking_ang_vel(self):
         """奖励yaw角速度跟踪，用于平衡车转向控制"""
