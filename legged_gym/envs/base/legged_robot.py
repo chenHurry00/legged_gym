abc636f8da362437346fcd1e51bb8fa7ddd5a028
yuchen <1163581142@qq.com>
2025-12-04 11:03:11 +0800

    Resolve issue of pri_obs assignment.

--- legged_gym/envs/base/legged_robot.py
+++ legged_gym/envs/base/legged_robot.py
@@ -207,7 +207,30 @@ class LeggedRobot(BaseTask):
             rew = self._reward_termination() * self.reward_scales["termination"]
             self.rew_buf += rew
             self.episode_sums["termination"] += rew
-    
+
+    # def compute_observations(self):
+    #     """ Computes observations
+    #     """
+    #     self.obs_buf = torch.cat((  # self.base_lin_vel * self.obs_scales.lin_vel,
+    #         self.base_ang_vel * self.obs_scales.ang_vel,
+    #         self.projected_gravity,
+    #         self.commands[:, :3] * self.commands_scale,
+    #         (self.dof_pos - self.default_dof_pos) * self.obs_scales.dof_pos,
+    #         self.dof_vel * self.obs_scales.dof_vel,
+    #         self.actions
+    #     ), dim=-1)
+    #
+    #     # add perceptive inputs if not blind
+    #     # if self.cfg.terrain.measure_heights:
+    #     #     heights = torch.clip(self.root_states[:, 2].unsqueeze(1) - 0.5 - self.measured_heights, -1,
+    #     #                          1.) * self.obs_scales.height_measurements
+    #     #     self.privileged_obs_buf = torch.cat(
+    #     #         (self.privileged_obs_buf, heights), dim=-1)
+    #
+    #     # add noise if needed
+    #     if self.add_noise:
+    #         self.obs_buf += (2 * torch.rand_like(self.obs_buf) - 1) * self.noise_scale_vec
+
     def compute_observations(self):
         """ Computes observations
         """
@@ -226,33 +249,16 @@ class LeggedRobot(BaseTask):
         self.obs_buf = torch.cat((current_obs[:, :self.num_proprio], self.obs_buf[:, :-self.num_proprio]),
                                  dim=-1)
 
-    def compute_privileged_observations(self):
-        """ Compute privileged observations.
-          """
-        energy_level = (self.w_prime_bal / self.cfg.metabolic.w_prime_total).unsqueeze(1)
-
-        current_obs = torch.cat((self.base_ang_vel * self.obs_scales.ang_vel,
-                                 self.projected_gravity,
-                                 self.commands[:, :3] * self.commands_scale,
-                                 (self.dof_pos - self.default_dof_pos) * self.obs_scales.dof_pos,
-                                 self.dof_vel * self.obs_scales.dof_vel,
-                                 self.actions,
-                                 ), dim=-1)
-
-        current_pri_obs = torch.cat((current_obs[:, :self.num_proprio],
+        current_pri_obs = torch.cat((current_obs,
                                              self.base_lin_vel * self.obs_scales.lin_vel,), dim=-1)
-
-        self.num_privileged_obs = current_pri_obs # torch.cat((current_pri_obs, self.num_privileged_obs[:, :-self.num_proprio+3]),dim=-1)
 
         if self.cfg.terrain.measure_heights:
             heights = torch.clip(self.root_states[:, 2].unsqueeze(1) - 0.5 - self.measured_heights, -1,
                                  1.) * self.obs_scales.height_measurements
-            self.privileged_obs_buf = torch.cat(
-                (self.privileged_obs_buf, heights), dim=-1)
+            current_pri_obs = torch.cat(
+                (current_pri_obs, heights), dim=-1)
 
-        # add noise if needed
-        if self.add_noise:
-            self.privileged_obs_buf += (2 * torch.rand_like(self.privileged_obs_buf) - 1) * self.noise_scale_vec
+        self.privileged_obs_buf = current_pri_obs
 
     def create_sim(self):
         """ Creates simulation, terrain and evironments
