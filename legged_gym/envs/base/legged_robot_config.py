bae04cae6b27e524b6992ba0577228fc2510fa4a
yuchen <1163581142@qq.com>
2025-12-10 19:55:49 +0800

    Update params.

--- legged_gym/envs/base/legged_robot_config.py
+++ legged_gym/envs/base/legged_robot_config.py
@@ -135,21 +135,25 @@ class LeggedRobotCfg(BaseConfig):
         max_push_vel_xy = 1.
 
         # 随机初始状态参数
-        domain_scale = 0.2          # 缩放比例，课程学习时以terrain_level=5为基准比例放大
-        randomize_start_pos = True      # 随机化初始位置
-        curriculum = True              # 课程学习的初始状态
-        start_pos_range = [-0.5, 0.5]   # 初始位置随机范围 (x, y)
-        randomize_start_height = True   # 随机化初始高度
-        start_height_range = [0.30, 0.5] # 初始高度随机范围 (z)
-        randomize_start_rot = True      # 随机化初始姿态
-        start_rot_range = [-1.73, 1.73]   # 初始姿态随机范围 (roll, pitch)
-        randomize_start_lin_vel = True  # 随机化初始线速度
-        start_lin_vel_range = [-0.1, 0.1] # 初始线速度随机范围
-        randomize_start_ang_vel = True  # 随机化初始角速度
-        start_ang_vel_range = [-0.2, 0.2] # 初始角速度随机范围
+        domain_scale = 0.2  # 缩放比例，课程学习时以terrain_level=5为基准比例放大
+        randomize_start_pos = False  # 随机化初始位置
+        curriculum = True  # 课程学习的初始状态
+        start_pos_range = [-0.5, 0.5]  # 初始位置随机范围 (x, y)
+        randomize_start_height = False  # 随机化初始高度
+        start_height_range = [0.30, 0.5]  # 初始高度随机范围 (z)
+        randomize_start_rot = True  # 随机化初始姿态
+        start_rot_range = [-1.73, 1.73]  # 初始姿态随机范围 (roll, pitch)
+        randomize_start_lin_vel = False  # 随机化初始线速度
+        start_lin_vel_range = [-0.1, 0.1]  # 初始线速度随机范围
+        randomize_start_ang_vel = False  # 随机化初始角速度
+        start_ang_vel_range = [-0.2, 0.2]  # 初始角速度随机范围
 
     class rewards:
+        # clearance_height_target = -0.22
         class scales:
+            # foot_clearance_up = -0.5
+            # foot_slide_up = -0.05
+            # stumble_up = -0.05
             termination = -0.0
             tracking_lin_vel = 1.0
             tracking_ang_vel = 0.5
@@ -165,6 +169,9 @@ class LeggedRobotCfg(BaseConfig):
             feet_stumble = -0.0
             action_rate = -0.01
             stand_still = -0.
+            # foot_mirror_up = -0.05
+            # stand_nice = -0.1
+            # feet_contact_forces = -0.00015
 
         only_positive_rewards = True  # if true negative total rewards are clipped at zero (avoids early termination problems)
         tracking_sigma = 0.25  # tracking reward = exp(-error^2/sigma)
